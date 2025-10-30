5cd539ab23d4a12f0c9d9666dec0a87b6bcc1854
Miltonr87 <miltonrodrigues713@gmail.com>
2025-10-29 23:58:20 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-
+// need to improve perfromance
 export default defineConfig({
   server: {
     host: "::",
